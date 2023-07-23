1d0603560b32392ad300504f65099380097df9be
Ярослав <ananas617@mail.ru>
2023-07-23 22:46:33 +0300

    Style fixes for moonboard_BLE_service.py and moonboard_app_protocol.py. Remove problems folder

--- ble/moonboard_BLE_service.py
+++ ble/moonboard_BLE_service.py
@@ -1,59 +1,64 @@
+import json
+import logging
+import os
+import pty
+import subprocess
 import sys
-import dbus, dbus.mainloop.glib
+import threading
+
+import dbus
+import dbus.mainloop.glib
 from gi.repository import GLib
-from gatt_base.gatt_lib_advertisement import Advertisement
+
+import gatt_base.gatt_lib_variables as gatt_var
 from gatt_base.gatt_lib_characteristic import Characteristic
 from gatt_base.gatt_lib_service import Service
-import string,json
-import subprocess
-import logging
 from moonboard_app_protocol import UnstuffSequence, decode_problem_string
 
-import os
-import threading
-import pty
- 
-BLUEZ_SERVICE_NAME =           'org.bluez'
-DBUS_OM_IFACE =                'org.freedesktop.DBus.ObjectManager'
-LE_ADVERTISING_MANAGER_IFACE = 'org.bluez.LEAdvertisingManager1'
-GATT_MANAGER_IFACE =           'org.bluez.GattManager1'
-GATT_CHRC_IFACE =              'org.bluez.GattCharacteristic1'
-UART_SERVICE_UUID =            '6e400001-b5a3-f393-e0a9-e50e24dcca9e'
-UART_RX_CHARACTERISTIC_UUID =  '6e400002-b5a3-f393-e0a9-e50e24dcca9e'
-UART_TX_CHARACTERISTIC_UUID =  '6e400003-b5a3-f393-e0a9-e50e24dcca9e'
-LOCAL_NAME =                   'Moonboard A'
-SERVICE_NAME=                  'com.moonboard'
-mainloop = None
+UART_SERVICE_UUID =             '6e400001-b5a3-f393-e0a9-e50e24dcca9e'
+UART_RX_CHARACTERISTIC_UUID =   '6e400002-b5a3-f393-e0a9-e50e24dcca9e'
+UART_TX_CHARACTERISTIC_UUID =   '6e400003-b5a3-f393-e0a9-e50e24dcca9e'
 
+LOCAL_NAME      = 'Moonboard ElCapitan'
+SERVICE_NAME    = 'com.moonboard'
+LOGGER_NAME     = 'moonboard'
+
+
+# NOTE Sometimes the second part of packet is lost. Therefore btmon is used
 class RxCharacteristic(Characteristic):
     def __init__(self, bus, index, service, process_rx):
-        Characteristic.__init__(self, bus, index, UART_RX_CHARACTERISTIC_UUID,
-                                ['write'], service)
-        self.process_rx=process_rx
+        Characteristic.__init__(self, bus, index, UART_RX_CHARACTERISTIC_UUID, ['write'], service)
+        self.process_rx = process_rx
 
+    @dbus.service.method(gatt_var.GATT_CHRC_IFACE, in_signature='aya{sv}')
     def WriteValue(self, value, options):
         pass
-        #self.process_rx(value)
+        self.process_rx(value)
+
 
 class UartService(Service):
-    def __init__(self, bus,path, index, process_rx):
-        Service.__init__(self, bus,path, index, UART_SERVICE_UUID, True)
-        self.add_characteristic(RxCharacteristic(bus, 1, self, process_rx))       
+    def __init__(self, bus, path, index, process_rx):
+        Service.__init__(self, bus, path, index, UART_SERVICE_UUID, True)
+        self.add_characteristic(RxCharacteristic(bus, 1, self, process_rx))
 
-class OutStream:
-    def __init__(self, fileno):
-        self._fileno = fileno
-        self._buffer = b""
+
+class DataStream:
+    def __init__(self, fileno: int):
+        self._fileno: int = fileno
+        self._buffer: bytes = b""
 
     def read_lines(self):
         try:
             output = os.read(self._fileno, 1000)
         except OSError as e:
-            if e.errno != errno.EIO: raise
+            if e.errno != errno.EIO:
+                raise
+
             output = b""
+
         lines = output.split(b"\n")
-        lines[0] = self._buffer + lines[0] # prepend previous
-                                           # non-finished line.
+        lines[0] = self._buffer + lines[0]  # prepend previous
+        # non-finished line.
         if output:
             self._buffer = lines[-1]
             finished_lines = lines[:-1]
@@ -66,63 +71,69 @@ class OutStream:
             finished_lines = lines
             readable = False
 
-        finished_lines = [line.rstrip(b"\r")
-                         for line in finished_lines]
-        
+        finished_lines = [line.rstrip(b"\r") for line in finished_lines]
+
         return finished_lines, readable
 
 
 class MoonApplication(dbus.service.Object):
     IFACE = "com.moonboard.method"
-    def __init__(self, bus, socket,logger):
-        self.path = '/com/moonboard'
-        self.services = []
-        self.logger=logger
-        self.unstuffer= UnstuffSequence(self.logger)
-        dbus.service.Object.__init__(self, bus, self.path)
-        self.add_service(UartService(bus,self.get_path(), 0, self.process_rx)) 
+    PATH = '/com/moonboard'
 
-        monitor_thread = threading.Thread(target=self.monitor_btmon)  
+    def __init__(self, bus: dbus.service.BusName, logger: logging.Logger):
+        super().__init__(bus, self.PATH)
+
+        self.services = []
+        self.logger = logger
+        self.unstuffer = UnstuffSequence(self.logger)
+
+        self.add_service(UartService(bus, self.get_path(), 0, self.process_rx))
+
+        monitor_thread = threading.Thread(target=self.monitor_btmon)
         monitor_thread.start()
 
-    def monitor_btmon(self): 
+    def monitor_btmon(self):
         out_r, out_w = pty.openpty()
-        cmd = ["sudo","btmon"]
-        process = subprocess.Popen(cmd, stdout=out_w)
-        f = OutStream(out_r)
+        cmd = ["sudo", "btmon"]
+        subprocess.Popen(cmd, stdout=out_w)
+
+        f = DataStream(out_r)
         while True:
             lines, readable = f.read_lines()
-            if not readable: break
-            for line in lines:                
-                if line != '':
-                    line = line.decode()
-                    if 'Data:' in line:
-                        data = line.replace(' ','').replace('\x1b','').replace('[0m','').replace('Data:','')
-                        self.process_rx(data)
-                        self.logger.info('New data '+ data)
+            if not readable:
+                break
 
-    def process_rx(self,ba):
-        new_problem_string= self.unstuffer.process_bytes(ba)
+            for line in lines:
+                if line == '':
+                    continue
+
+                line = line.decode()
+                if 'Data:' in line:
+                    data = line.replace(' ', '').replace('\x1b', '').replace('[0m', '').replace('Data:', '')
+                    self.logger.info('New data ' + data)
+                    self.process_rx(data)
+
+    def process_rx(self, ba):
+        new_problem_string = self.unstuffer.process_bytes(ba)
         flags = self.unstuffer.flags
 
         if new_problem_string is not None:
-            problem= decode_problem_string(new_problem_string, flags)
+            problem = decode_problem_string(new_problem_string, flags)
             self.new_problem(json.dumps(problem))
-            self.unstuffer.flags = ''
+            self.unstuffer.reset()
             start_adv(self.logger)
 
-    @dbus.service.signal(dbus_interface="com.moonboard",
-                            signature="s")
-    def new_problem(self, problem):
-        self.logger.info('Signal new problem: '+ str(problem))
+    @dbus.service.signal(dbus_interface="com.moonboard", signature="s")
+    def new_problem(self, problem: str):
+        self.logger.info('Signal new problem: ' + str(problem))
 
     def get_path(self):
-        return dbus.ObjectPath(self.path)
+        return dbus.ObjectPath(self.PATH)
 
     def add_service(self, service):
         self.services.append(service)
 
-    @dbus.service.method(DBUS_OM_IFACE, out_signature='a{oa{sa{sv}}}')
+    @dbus.service.method(gatt_var.DBUS_OM_IFACE, out_signature='a{oa{sa{sv}}}')
     def GetManagedObjects(self):
         response = {}
         for service in self.services:
@@ -132,120 +143,101 @@ class MoonApplication(dbus.service.Object):
                 response[chrc.get_path()] = chrc.get_properties()
         return response
 
-def register_app_cb():
-    print('GATT application registered')
-
-
-def register_app_error_cb(error):
-    print('Failed to register application: ' + str(error))
-    mainloop.quit()
-
 
 def run(*popenargs, **kwargs):
-    input = kwargs.pop("input", None)
-    check = kwargs.pop("handle", False)
-
-    if input is not None:
-        if 'stdin' in kwargs:
-            raise ValueError('stdin and input arguments may not both be used.')
-        kwargs['stdin'] = subprocess.PIPE
-
     process = subprocess.Popen(*popenargs, **kwargs)
+
     try:
-        stdout, stderr = process.communicate(input)
-    except:
+        stdout, stderr = process.communicate()
+    except Exception:
         process.kill()
         process.wait()
         raise
+
     retcode = process.poll()
-    if check and retcode:
-        raise subprocess.CalledProcessError(
-            retcode, process.args, output=stdout, stderr=stderr)
+
     return retcode, stdout, stderr
 
 
-def setup_adv(logger):
+def setup_adv(logger: logging.Logger):
     logger.info('setup adv')
-    setup_adv = [
-    "hcitool -i hci0 cmd 0x08 0x000a 00",
-    "hcitool -i hci0 cmd 0x08 0x0008 18 02 01 06 02 0a 00 11 07 9e ca dc 24 0e e5 a9 e0 93 f3 a3 b5 01 00 40 6e 00 00 00 00 00 00 00",
-    "hcitool -i hci0 cmd 0x08 0x0009 0d 0c 09 4d 6f 6f 6e 62 6f 61 72 64 20 41",
-    "hcitool -i hci0 cmd 0x08 0x0006 80 02 c0 03 00 00 00 00 00 00 00 00 00 07 00"
+
+    setup_adv_commands = [
+        "hcitool -i hci0 cmd 0x08 0x000a 00",
+        "hcitool -i hci0 cmd 0x08 0x0008 18 02 01 06 02 0a 00 11 07 9e ca dc 24 0e e5 a9 e0 93 f3 a3 b5 01 00 40 6e 00 00 00 00 00 00 00",
+        "hcitool -i hci0 cmd 0x08 0x0009 0d 0c 09 4d 6f 6f 6e 62 6f 61 72 64 20 41",
+        "hcitool -i hci0 cmd 0x08 0x0006 80 02 c0 03 00 00 00 00 00 00 00 00 00 07 00"
     ]
-    for c in setup_adv:
-        run("sudo "+ c, shell=True)
+
+    for c in setup_adv_commands:
+        run("sudo " + c, shell=True)
 
 
-def start_adv(logger,start=True):
+def start_adv(logger, start=True):
     if start:
-        start='01'
+        start = '01'
         logger.info('start adv')
     else:
-        start='00'
+        start = '00'
         logger.info('stop adv')
-    start_adv= "hcitool -i hci0 cmd 0x08 0x000a {}".format(start)
-    run("sudo " +start_adv, shell=True)
 
-def main(logger,adapter):
-    global mainloop
+    start_adv_command = f"hcitool -i hci0 cmd 0x08 0x000a {start}"
 
-    logger.info("Bluetooth adapter: "+ str(adapter))
+    run("sudo " + start_adv_command, shell=True)
+
+
+def main(logger: logging.Logger, adapter: str):
+    logger.info("Bluetooth adapter: " + str(adapter))
 
     dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
     bus = dbus.SystemBus()
 
     try:
-        bus_name = dbus.service.BusName(SERVICE_NAME,
-                                        bus=bus,
-                                        do_not_queue=True)
+        bus_name = dbus.service.BusName(SERVICE_NAME, bus=bus, do_not_queue=True)
     except dbus.exceptions.NameExistsException:
+        logger.critical(f"Bus name {SERVICE_NAME} already exists")
         sys.exit(1)
 
-    app = MoonApplication(bus_name,None,logger)    
+    app = MoonApplication(bus_name, logger)
 
-    service_manager = dbus.Interface(
-                                bus.get_object(BLUEZ_SERVICE_NAME, adapter),
-                                GATT_MANAGER_IFACE)
+    service_manager = dbus.Interface(bus.get_object(gatt_var.BLUEZ_SERVICE_NAME, adapter), gatt_var.GATT_MANAGER_IFACE)
 
- 
     loop = GLib.MainLoop()
 
-    logger.info('app path: '+ app.get_path())
+    logger.info('app path: ' + app.get_path())
 
     service_manager.RegisterApplication(app.get_path(), {},
-                                        reply_handler=register_app_cb,
-                                        error_handler=register_app_error_cb)
-    
+                                        reply_handler=lambda: print("GATT application registered"),
+                                        error_handler=lambda error: print(f"Failed to register application: {error}"))
+
     setup_adv(logger)
-    start_adv(logger)
+    start_adv(logger, True)
 
     # Run the loop
     try:
         loop.run()
     except KeyboardInterrupt:
-        print("keyboard interrupt received")
+        print("Keyboard interrupt received")
     except Exception as e:
         print("Unexpected exception occurred: '{}'".format(str(e)))
     finally:
         loop.quit()
 
- 
+
 if __name__ == '__main__':
-    
     import argparse
+
     parser = argparse.ArgumentParser(description='Moonboard bluetooth service')
-    parser.add_argument('--debug',  action = "store_true")
+    parser.add_argument('--debug', action="store_true")
 
     args = parser.parse_args()
-    argsd=vars(args)
 
-    logger = logging.getLogger('moonboard.ble')
-    logger.setLevel(logging.DEBUG)
-    logger.addHandler(logging.StreamHandler())
+    moon_logger = logging.getLogger(LOGGER_NAME)
+    moon_logger.addHandler(logging.StreamHandler())
 
     if args.debug:
-        logger.setLevel(logging.DEBUG)
+        moon_logger.setLevel(logging.DEBUG)
     else:
-        logger.setLevel(logging.INFO)
+        moon_logger.setLevel(logging.INFO)
 
-    main(logger,adapter='/org/bluez/hci0')
+    main(moon_logger, adapter='/org/bluez/hci0')
--- ble/moonboard_app_protocol.py
+++ ble/moonboard_app_protocol.py
@@ -3,82 +3,90 @@ import string
 
 X_GRID_NAMES = string.ascii_uppercase[0:11]
 
-def position_trans(p,num_rows):
-    """convert led number (strip number) to moonboard grid """
-    col= p//num_rows
-    row= (p%num_rows) +1
-    if col%2==1:
-        row=(num_rows+1)-row
-    return X_GRID_NAMES[col]+str(row)
-    
-def decode_problem_string(s, flags):
-    holds = {'START':[],'MOVES':[],'TOP':[], 'FLAGS':[flags]}
 
-    if flags.find("M") != -1:
-        num_rows = 12
-    else:
-        num_rows = 18
+def position_trans(pos, num_rows: int):
+    """convert led number (strip number) to moonboard grid """
+    col = pos // num_rows
+    row = (pos % num_rows) + 1
+
+    if col % 2 == 1:
+        row = (num_rows + 1) - row
+    return X_GRID_NAMES[col] + str(row)
+
+
+def decode_problem_string(s: str, flags: str):
+    holds = {'START': [], 'MOVES': [], 'TOP': [], 'FLAGS': [flags]}
+
+    num_rows = 18 if flags.find("M") == -1 else 12
 
     for h in s.split(','):
-        t,p = h[0],position_trans(int(h[1:]), num_rows)
-        if t=='S':
-            holds['START'].append(p)
-        if t=='P':
-            holds['MOVES'].append(p)
-        if t=='E':
-            holds['TOP'].append(p)
+        type, position = h[0], position_trans(int(h[1:]), num_rows)
+        if type == 'S':
+            holds['START'].append(position)
+        elif type == 'P':
+            holds['MOVES'].append(position)
+        elif type == 'E':
+            holds['TOP'].append(position)
+
     return holds
 
-class UnstuffSequence():
+
+class UnstuffSequence:
     """
     hold sequence come separated in parts due to BLE packet size limitation
     this class serves to put different parts together
     """
+
     START = 'l#'
-    STOP= '#'
+    STOP = '#'
 
-    def __init__(self,logger=None):
-        if logger is None:
-            self.logger= logging
-        else:
-            self.logger=logger
-        self.s=''
-        self.flags=''
+    def __init__(self, logger: logging.Logger = None):
+        self.logger = logger if logger is not None else logging
 
-    def process_bytes(self, ba):
+        self.s: str = ''
+        self.flags: str = ''
+        self.started: bool  = False
+
+    def process_bytes(self, ba: str):
         """ 
         process new incoming bytes and return if new problem is available. 
         handle some error due to multiple connected devices sending simoultaneously.
         """
 
         s = bytearray.fromhex(ba).decode()
-        self.logger.debug("incoming bytes:"+str(s))
-        
+        self.logger.debug("incoming bytes:" + str(s))
+
         if s[0] == '~' and s[-1] == '*':
             # Flag processing
-            self.flags=s[1:-1]
+            self.flags = s[1:-1]
             if s.find("M") != -1:
                 self.logger.debug('MINI')
             if s.find("D") != -1:
                 self.logger.debug('BothLights')
-        elif s[:2]==self.START:
+        elif s[:2] == self.START:
             self.logger.debug('START')
-            if self.s =='':
-                if s[-1]==self.STOP:
+            if not self.started:
+                if s[-1] == self.STOP:
                     return s[2:-1]
                 else:
-                    self.s=s[2:]
+                    self.started = True
+                    self.s = s[2:]
             else:
                 self.logger.debug('error: alredy started')
-                self.s= ''
-        elif s[-1]==self.STOP:
+                self.reset()
+        elif s[-1] == self.STOP:
             self.logger.debug('STOP')
-            if self.s!='':
-                ret = self.s+s[:-1]
-                self.s=''
+            if self.started:
+                ret = self.s + s[:-1]
+                self.reset()
                 return ret
             else:
                 self.logger.debug('error: not started')
-                self.s= ''
+                self.reset()
         else:
-            self.s+=s
+            if self.started:
+                self.s += s
+
+    def reset(self):
+        self.s = ''
+        self.started = False
